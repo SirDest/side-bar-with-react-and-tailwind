c8c2545eb9c1b1fd5b41e5ffc342e4ed00b08a8c
Akinro Destined <104671422+SirDest@users.noreply.github.com>
2024-09-09 12:52:30 +0100

    Initial commit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -51,15 +51,21 @@ const SideBar = ({ children, expanded, setExpanded }) => {
   );
 };
 
-export const SideBarItem = ({ icon, text, active, alert }) => {
+export function SideBarItem({ icon, text, active, alert }) {
   const { expanded } = useContext(SidebarContext);
+
   return (
     <li
-      className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors ${
-        active
-          ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
-          : "hover:bg-indigo-50 text-gray-600"
-      }`}
+      className={`
+        relative flex items-center py-2 px-3 my-1
+        font-medium rounded-md cursor-pointer
+        transition-colors group
+        ${
+          active
+            ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+            : "hover:bg-indigo-50 text-gray-600"
+        }
+    `}
     >
       {icon}
       <span
@@ -76,15 +82,21 @@ export const SideBarItem = ({ icon, text, active, alert }) => {
           }`}
         />
       )}
+
       {!expanded && (
         <div
-          className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:opacity-100 group-hover:translate-x-0`}
+          className={`
+          absolute left-full rounded-md px-2 py-1 ml-6
+          bg-indigo-100 text-indigo-800 text-sm
+          invisible opacity-20 -translate-x-3 transition-all
+          group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+      `}
         >
           {text}
         </div>
       )}
     </li>
   );
-};
+}
 
 export default SideBar;
